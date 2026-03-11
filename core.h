3b8274b2decabf18567babff4445adbb6b2a8d13
grant-tm <grant.t.morgan@gmail.com>
2026-03-11 11:05:32 -0500

    create umbrella header and typedefs

---
+++ core.h
@@ -0,0 +1,6 @@
+#ifndef CORE_H
+#define CORE_H
+
+#include "core_types.h"
+
+#endif // CORE_H
---
+++ core_types.h
@@ -0,0 +1,34 @@
+#ifndef CORE_TYPES_H
+#define CORE_TYPES_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <uchar.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef ptrdiff_t isize;
+typedef size_t usize;
+
+typedef float f32;
+typedef double f64;
+
+typedef unsigned char byte;
+typedef unsigned char uchar;
+typedef i32 b32;
+
+typedef char c8;
+typedef char16_t c16;
+typedef char32_t c32;
+typedef wchar_t wchar;
+
+#endif // CORE_TYPES_H
